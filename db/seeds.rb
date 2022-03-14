757b3fcc51ec5763b5d6af37401c994e7e59ca46
PierreLgx <97539842+Pierrooow@users.noreply.github.com>
2022-03-14 10:24:56 +0100

    creation BDD

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+10.times do
+    item = Item.create!(
+        title: Faker::Creature::Cat.name, #=> "Shadow"
+        description: Faker::Creature::Cat.breed, #=> "British Semipi-longhair"
+        price: rand(1..99),
+        image_url: "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"
+    )
+    end

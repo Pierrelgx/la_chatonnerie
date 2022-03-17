9bb4675d214e07bb60ad3ac99539ff44d3fbf80d
PierreLgx <97539842+Pierrooow@users.noreply.github.com>
2022-03-17 11:37:59 +0100

    ajout visuel bouton + ajout def

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,12 @@
 class Order < ApplicationRecord
   belongs_to :user
+
+  after_create :order_send 
+
+  def order_send 
+    UserMailer.order_mail(self).deliver_now
+    AdminMailer.order_mail(self).deliver_now
+  end
+
+
 end
